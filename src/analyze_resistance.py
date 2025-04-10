86e018b6e435d963e6392a271331ca7d09c0408f
Michał Krawczyk <michal.krawczyk.rogi@gmail.com>
2025-04-10 23:27:40 +0200

    Changed graph is displayed

--- src/analyze_resistance.py
+++ src/analyze_resistance.py
@@ -1,4 +1,7 @@
+import os
+
 import networkx as nx
+from matplotlib import pyplot as plt
 
 from src.graph_operations import graph_operations
 
@@ -7,6 +10,12 @@ def analyze_graph_resistance(graph: nx.Graph, folder_path, name=""):
     graph_copy = graph.copy()
     hubs = sorted(graph_copy.degree, key=lambda x: x[1], reverse=True)[:5]
     graph_copy.remove_nodes_from([node for node, degree in hubs])
+
+    plt.title(name)
+    nx.draw(graph_copy, with_labels=True)
+    plt.savefig(os.path.join(folder_path, "graph_r.png"))
+    plt.show()
+    plt.close()
 
     print(f"--< {name} - with some nodes deleted >------------------------------------------------")
     return graph_operations(graph_copy, folder_path, name, "_r")
